365d7d489b72630703a0e4690e5431f47539442b
Park Junyoung <bloomwayz@snu.ac.kr>
2025-10-15 10:53:26 +0900

    ➕ Use React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <p>Hello World!</p>
-      <button onClick={() => setCount((count) => count + 1)}>
-        Count is {count}
-      </button>
+      { /* TODO: Add a header */ }
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+const SignInPage = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Hello SignInPage!</p>
+      <button
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        Count is {count}
+      </button>
+    </div>
+  );
+};
+
+export default SignInPage;
