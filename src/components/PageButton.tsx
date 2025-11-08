705d88eef28396ba578cb627f37ca0a8688f81ff
Park Junyoung <bloomwayz@snu.ac.kr>
2025-11-09 04:03:48 +0900

    💄 Add hover style to PageButtons

--- src/components/PageButton.tsx
+++ src/components/PageButton.tsx
@@ -6,7 +6,10 @@ type PageButtonProp = {
 
 const PageButton = ({ content, isSelected, onClick }: PageButtonProp) => {
   return isSelected ? (
-    <button className="bg-[#383b41] rounded-md w-8 h-8 text-sm text-white" onClick={onClick}>
+    <button
+      className="bg-[#383b41] rounded-md w-8 h-8 text-sm text-white hover:bg-gray-300"
+      onClick={onClick}
+    >
       {content}
     </button>
   ) : (
