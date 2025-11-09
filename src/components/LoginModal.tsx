d51b24ed7c8c033c00b9c704f91b90ef092f0220
Park Junyoung <bloomwayz@snu.ac.kr>
2025-11-09 15:46:25 +0900

    💄 Update login modal UI

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -21,29 +21,29 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
       onClick={onClose}
     >
       <div
-        className="bg-white rounded-lg p-6 max-w-sm w-full mx-4"
+        className="bg-white rounded-2xl p-8 max-w-sm w-full mx-4"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="text-center">
-          <h2 className="text-xl font-bold mb-4">
+          <h2 className="text-md text-[#383b41] font-bold mb-4">
             찜하기를 하려면 로그인이 필요해요
           </h2>
-          <p className="text-gray-600 mb-6">
+          <p className="text-sm text-[#383b41] mb-8 leading-[1.5]">
             계정이 없으시다면 <br></br> 지금 바로 회원가입해보세요
           </p>
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-2">
           <button
             onClick={handleLogin}
-            className="w-full px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-800"
+            className="w-full px-4 py-2 bg-[#484c53] text-white text-xs font-semibold rounded-lg hover:bg-gray-800"
           >
-            로그인
+            로그인하기
           </button>
           <button
             onClick={onClose}
-            className="w-full px-4 py-2 bg-gray-200 text-gray-700 rounded hover:bg-gray-300"
+            className="w-full px-4 py-2 bg-[#f1f2f5] text-[#777f8b] text-xs font-semibold rounded-lg hover:bg-gray-300"
           >
-            뒤로가기
+            뒤로 가기
           </button>
         </div>
       </div>
