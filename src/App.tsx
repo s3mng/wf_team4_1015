a9da9f23f3738efd53f6492a60361aa509743a61
s3mng <104117184+s3mng@users.noreply.github.com>
2025-11-20 16:25:31 +0900

    mypg

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import { AuthProvider } from './contexts/AuthContext';
 import MainPage from './pages/MainPage';
+import MyPage from './pages/MyPage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+
 
 const App = () => {
   return (
@@ -15,6 +17,7 @@ const App = () => {
             <Route path="/" element={<MainPage />} />
             <Route path="/sign-up" element={<SignUpPage />} />
             <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/mypage" element={<MyPage />} />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
