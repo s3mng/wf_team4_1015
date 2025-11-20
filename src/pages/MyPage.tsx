de8ac0f003d3470deeb0c6cd3e7eaf821c007004
PARK Junyoung <bloomwayz@snu.ac.kr>
2025-11-20 19:44:36 +0900

    :sparkles: MyPage tab menu UI (#29)
    ## 요약
    
    - 마이페이지의 탭 메뉴 UI를 구현하였습니다. (#25)
    
    ---------
    
    Co-authored-by: s3mng <104117184+s3mng@users.noreply.github.com>

---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import BookmarkTab from '../tabs/BookmarkTab'
+import ProfileTab from '../tabs/ProfileTab'
+
+const MyPage = () => {
+  const [activeTab, setActiveTab] = useState<'bookmarks' | 'profile'>('bookmarks');
+
+  return (
+    <div className="mx-auto flex w-full max-w-[698px] flex-col gap-10 px-6 py-[50px]">
+      <h1 className="text-2xl font-bold">마이페이지</h1>
+
+      {/* Tab list */}
+      <div className="flex gap-10">
+        <button
+          onClick={() => setActiveTab('bookmarks')}
+          className={`transition-colors text-lg ${
+            activeTab === 'bookmarks'
+              ? 'pb-0.5 border-b-2 border-black text-black font-bold'
+              : 'pb-1 font-medium'
+          }`}
+        >
+          관심공고
+        </button>
+
+        <button
+          onClick={() => setActiveTab('profile')}
+          className={`transition-colors text-lg ${
+            activeTab === 'profile'
+              ? 'pb-0.5 border-b-2 border-black text-black font-bold'
+              : 'pb-1 font-medium'
+          }`}
+        >
+          내 정보
+        </button>
+      </div>
+
+      {/* Tab contents */}
+      <div>
+        {activeTab === 'bookmarks' && (
+          <BookmarkTab />
+        )}
+        {activeTab === 'profile' && (
+          <ProfileTab />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MyPage;
---
+++ src/tabs/ProfileTab.tsx
@@ -0,0 +1,10 @@
+const ProfileTab = () => {
+  return (
+    <div className="w-full flex flex-col">
+      {/* TODO: Implement the profile tab */}
+      <p>프로필 탭</p>
+    </div>
+  );
+};
+
+export default ProfileTab;
