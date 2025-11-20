f625f385ceb94abbb8bd1b5fb1a97baf0cb9dd0b
Park Junyoung <bloomwayz@snu.ac.kr>
2025-11-21 08:14:58 +0900

    ✨ Profile tab w/o profile

--- src/tabs/NoProfileTab.tsx
+++ src/tabs/NoProfileTab.tsx
@@ -1,8 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+
 const NoProfileTab = () => {  
+  const navigate = useNavigate();
+
+  // NOTE: '/make-profile' page doesn't exist yet
+  const handleMakeProfile = () => {
+    navigate('/make-profile');
+  }
+  
   return (
-    <div className="w-full flex flex-col">
-      {/* TODO: Implement the profile tab */}
-      <p>프로필 탭(프로필 없는 경우)</p>
+    <div className="px-4 py-8 flex flex-col gap-6 place-content-center">
+      <h2 className="text-center text-2xl font-bold">
+        아직 프로필이 등록되지 않았어요!
+      </h2>
+      <p className="text-center text-md font-medium text-[#5f656f]">
+        기업에 소개할 나의 정보를 작성해서 나를 소개해보세요.
+      </p>
+      <button
+        onClick={handleMakeProfile}
+        className="py-2 bg-[#383b41] text-[#e8ebef] text-md font-bold rounded-lg hover:bg-gray-300"
+      >
+        지금 바로 프로필 작성하기
+      </button>
     </div>
   );
 };
